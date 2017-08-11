f11faae9fd1f70b568a51af1a550d97bc2e8b083
Victor Ampuero <vampuero@paperlessla.com>
2017-08-11 18:18:44 -0400

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,30 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log(`Unable to connect to MongoDB server`);
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({_id: new ObjectID('598de3b6c89ada0e9b82fdee')}).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Todos').find().count().then((count) => {
+    console.log(`Todos count: ${count}`);
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+  db.collection('Users').find({name: 'Victor'}).toArray().then((users) => {
+    console.log(JSON.stringify(users, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch users', err);
+  });
+
+  // db.close();
+});
