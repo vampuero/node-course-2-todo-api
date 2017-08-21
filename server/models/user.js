f275961c77031526faef54cd0f553919a7496506
Victor Ampuero <vampuero@paperlessla.com>
2017-08-21 14:26:16 -0300

    Server files

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+  email: {
+    required: true,
+    type: String,
+    trim: true,
+    minlength: 1
+  }
+});
+
+module.exports = {User};
