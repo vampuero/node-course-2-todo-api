1ba73628912cc3b4670b57465f3f74e9b7c608a1
Victor Ampuero <vampuero@paperlessla.com>
2017-08-21 16:56:20 -0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -10,12 +10,22 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
+
   var todo = new Todo({
     text: req.body.text
   });
 
   todo.save().then((doc)=> {
     res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   });
