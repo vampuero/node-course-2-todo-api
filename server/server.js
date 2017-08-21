6511cacfbfbc699907ebf2178c483a81b3422c4b
Victor Ampuero <vampuero@paperlessla.com>
2017-08-21 18:34:09 -0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+var {ObjectID} = require('mongodb');
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -30,6 +32,25 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 
+});
+
+// GET /todos/123123
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }, (e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(3000, () => {
