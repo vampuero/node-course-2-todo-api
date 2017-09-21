43af78cc9927349e04f5467fd69e7dac080b188c
Victor Ampuero <vampuero@paperlessla.com>
2017-09-21 17:50:15 -0300

    Add tests for DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -130,7 +130,7 @@ app.post('/users/login', (req, res) => {
 
 app.delete('/users/me/token', authenticate, (req, res) => {
   req.user.removeToken(req.token).then(() => {
-    req.status(200).send();
+    res.status(200).send();
   }, () => {
     res.status(400).send();
   });
